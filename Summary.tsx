ecc522cd06f705aca4010f843118135082e0b156
Geoyeob Kim <gasbank@gmail.com>
2023-04-18 01:21:40 +0900

    미보유중인 종목은 요약에서 빼기 (나중에 옵션 필요할 듯)

--- Summary.tsx
+++ Summary.tsx
@@ -5,22 +5,24 @@ import {View} from 'react-native';
 
 export function Summary(props: SummaryProps): JSX.Element {
   console.log(`props.summaryDict.size = ${props.summaryDict.size}`);
-  const historyEntryList = Array.from(props.summaryDict.entries()).map(e => {
-    return (
-      <SummaryEntry
-        key={e[1].stockName}
-        stockName={e[1].stockName}
-        stockCount={e[1].stockCount}
-        accumPrice={e[1].accumPrice}
-        accumBuyPrice={e[1].accumBuyPrice}
-        accumBuyCount={e[1].accumBuyCount}
-        accumSellPrice={e[1].accumSellPrice}
-        accumSellCount={e[1].accumSellCount}
-        accumEarn={e[1].accumEarn}
-        onSelect={props.onSelect}
-      />
-    );
-  });
+  const historyEntryList = Array.from(props.summaryDict.entries())
+    .filter(e => e[1].stockCount > 0)
+    .map(e => {
+      return (
+        <SummaryEntry
+          key={e[1].stockName}
+          stockName={e[1].stockName}
+          stockCount={e[1].stockCount}
+          accumPrice={e[1].accumPrice}
+          accumBuyPrice={e[1].accumBuyPrice}
+          accumBuyCount={e[1].accumBuyCount}
+          accumSellPrice={e[1].accumSellPrice}
+          accumSellCount={e[1].accumSellCount}
+          accumEarn={e[1].accumEarn}
+          onSelect={props.onSelect}
+        />
+      );
+    });
 
   return (
     <>
